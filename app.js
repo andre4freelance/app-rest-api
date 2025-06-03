63589b0b56faee50018b3263519b6a08650a36ae
“andre4freelance” <andre4freelance@gmail.com>
2025-06-03 15:34:43 +0000

    Add arithmetic functions and update package.json scripts

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+const penjumlahan = (a, b) => {
+  return a + b;
+}
+const pengurangan = (a, b) => {
+  return a - b;
+}
+console.log(penjumlahan(2, 44)); // Output: 8
+console.log(pengurangan(55, 61)); // Output: 2
+console.log("Hello, World!"); // Output: Hello, World!
