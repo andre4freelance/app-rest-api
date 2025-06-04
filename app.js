ed890e51e9ab4e4387b61c8aabd203d3277c43e4
“andre4freelance” <andre4freelance@gmail.com>
2025-06-04 09:48:18 +0000

    install express, mysql dan bodyPraser

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const penjumlahan = (a, b) => {
-  return a + b;
-}
-const pengurangan = (a, b) => {
-  return a - b;
-}
-console.log(penjumlahan(2, 44)); // Output: 8
-console.log(pengurangan(55, 61)); // Output: 2
-console.log("Hello, World!"); // Output: Hello, World!
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+//Koneksi ke /public/ untuk tampilan web
+app.use(express.static('public'));
+
+// Middleware untuk parsing JSON
+app.use(bodyParser.json());
+
+// Middleware untuk parsing URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Koneksi ke database MySQL
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'Masuklah123', // Ganti dengan password MySQL Anda
+    database: 'my_database' // Ganti dengan nama database Anda
+});
+
+// Membuat koneksi ke database
+db.connect((err) => {
+    if (err) {
+        console.error('Koneksi ke database gagal:', err);
+        return;
+    }
+    console.log('Koneksi ke database berhasil');
+});     
+
+// Menjalankan server pada port 8080
+const port = 8080;
+app.listen(port, () => {
+    console.log(`http://192.168.100.209:${port}`);
+});
