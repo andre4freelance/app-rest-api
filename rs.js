f070fc77fbbab9fe9c2648adb9c7750e35a09c23
“andre4freelance” <andre4freelance@gmail.com>
2025-06-07 14:17:39 +0000

    refactor: enhance response function documentation in rs.js

--- rs.js
+++ rs.js
@@ -1,3 +1,11 @@
-`use strict`;
+`use strict`; // Use strict digunakan untuk mengaktifkan mode ketat pada JavaScript
 
-export.ok = 
+// Function untuk mengirimkan response dengan status 200 dan data values
+exports.ok = function(values, res) {
+    var data = {
+        status: '200',
+        values: values
+    }
+    res.json(data);
+    res.end();
+}
