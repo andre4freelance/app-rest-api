66da4d519c56110a060c36743194f921aec3c11e
“andre4freelance” <andre4freelance@gmail.com>
2025-06-07 16:38:47 +0000

    feat: add route for fetching all items from the database

--- controller.js
+++ controller.js
@@ -1,8 +1,20 @@
 'use strict'; // Use strict digunakan untuk mengaktifkan mode ketat pada JavaScript
 var response = require('./res'); // Import module response untuk mengirimkan response
 var dbConn = require('./db'); // Import koneksi database
+const e = require('express');
 
 // Fungsi untuk mengirimkan response dengan status 200 dan pesan "REST API running well!"
 exports.index = function(req, res) {
     response.ok("REST API running well!", res)
+};
+
+// Menampilkan semua data dari tabel barang
+exports.tampilkanSemuaBarang = function(req, res) {
+    dbConn.query('SELECT * FROM barang', function(error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        }else {
+            response.ok(rows, res);
+        }
+    });
 };
--- routes.js
+++ routes.js
@@ -6,4 +6,8 @@ module.exports = function(app) {
 
     app.route('/API')
         .get(myjson.index);
+
+    app.route('/API/barang')
+        .get(myjson.tampilkanSemuaBarang);
 };
+
