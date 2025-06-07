83ef47174bc4426a8a7b69761cc5c14a1ef7d059
“andre4freelance” <andre4freelance@gmail.com>
2025-06-07 14:14:50 +0000

    refactor: restructure application by moving main logic to main.js and updating database connection handling

--- db.js
+++ db.js
@@ -1,21 +1,19 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require('mysql'); // Import library mysql untuk koneksi ke database
+require('dotenv').config(); // Import dotenv untuk mengelola environment variables
 
 // Buat koneksi ke database
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
-// // Cek koneksi ke database
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Koneksi ke database gagal:', err);
-//     return;
-//   }
-//   console.log('Koneksi ke database berhasil');
-// }); 
+// Coba koneksi ke database
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the database successfully!');
+});
 
-module.exports = connection; // Export koneksi untuk digunakan di file lain
+// Export koneksi untuk digunakan di file lain
+module.exports = db ; 
--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const express = require('express'); // Untuk membuat webserver
 const app = express(); // Membuat instance dari express
 const bodyParser = require('body-parser'); // Untuk parsing request body
-const connection = require('./db'); // Mengimpor koneksi database dari db.js
-require('dotenv').config(); // Untuk mengakses environment dari file .env
+
 
 //Koneksi ke /public/ untuk tampilan web
-app.use(express.static('public'));
+// app.use(express.static('public'));
+
+// Middleware untuk parsing URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middleware untuk parsing JSON
 app.use(bodyParser.json());
 
-// Middleware untuk parsing URL-encoded data
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // Menjalankan webserver pada port 8080
 const port = 8080;
